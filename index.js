a639667aa78cc2e2d54127bcfcd5041db6bd10d3
programmer812 <64325806+programmer812@users.noreply.github.com>
2022-09-01 09:55:35 -0600

    Create personal website

---
+++ index.js
@@ -0,0 +1,16 @@
+// See More Button in Projects Section
+
+var counter = 0;
+
+$(".see-more-button").on("click", () => {
+    if (counter % 2 === 0) {
+        $(".see-more-button").html("<i class='fa-solid fa-caret-up'></i>");
+        $(".see-more-text").html("See Less");
+        $(".extra-cards").slideDown().removeClass("more-cards");
+    } else {
+        $(".see-more-button").html("<i class='fa-solid fa-caret-down'></i>");
+        $(".see-more-text").html("See More");
+        $(".extra-cards").slideUp().addClass("more-cards");
+    }
+    counter++;
+})
